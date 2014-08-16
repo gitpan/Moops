d63206622e36a830dddad45c38cb3c6f16ee6c7d
Toby Inkster <tobyink@cpan.org>
2014-08-16 20:53:58 +0000

    Import of TOBYINK/Moops-0.033 from CPAN.
    gitpan-cpan-distribution: Moops
    gitpan-cpan-version:      0.033
    gitpan-cpan-path:         TOBYINK/Moops-0.033.tar.gz
    gitpan-cpan-author:       TOBYINK
    gitpan-cpan-maturity:     released

--- lib/Moops/Keyword.pm
+++ lib/Moops/Keyword.pm
@@ -6,7 +6,7 @@ no warnings qw(void once uninitialized numeric);
 package Moops::Keyword;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.032';
+our $VERSION   = '0.033';
 
 use Moo;
 use B qw(perlstring);
@@ -56,15 +56,20 @@ sub generate_code
 	
 	# Stuff that must happen at runtime rather than compile time
 	$inject .= "'Moops'->at_runtime('$package');";
-
+	
+	my @guarded = @{ $self->_guarded };
 	state $i = 0;
-	$inject .= sprintf(
-		'my $__GUARD__%d_%d = bless(sub { %s }, "Moops::Keyword::__GUARD__");',
-		++$i,
-		100_000 + int(rand 899_000),
-		join(q[;], @{$self->_guarded}),
-	);
-
+	if (@guarded)
+	{
+		require Scope::Guard;
+		$inject .= sprintf(
+			'my $__GUARD__%d_%d = Scope::Guard->new(sub { %s });',
+			++$i,
+			100_000 + int(rand 899_000),
+			join(q[;], @guarded),
+		);
+	}
+	
 	return $inject;
 }
 
@@ -109,14 +114,19 @@ sub generate_package_setup_methods
 sub generate_type_constraint_setup
 {
 	my $self = shift;
-	require Type::Registry;
 	return map {
 		my $lib = use_package_optimistically($_);
 		$lib->isa('Type::Library')
-			? "use $lib -types; BEGIN { 'Type::Registry'->for_me->add_types(q[$lib]) };" :
-		$lib->can('type_names')
-			? "use $lib ('$lib'->type_names); BEGIN { 'Type::Registry'->for_me->add_types(q[$lib]) };" :
-		do { require Carp; Carp::croak("'$lib' is not a type constraint library") };
+			? "use $lib -types;"
+			: $lib->can('type_names')
+				? do {
+					require Type::Registry;
+					"use $lib ('$lib'->type_names); BEGIN { 'Type::Registry'->for_me->add_types(q[$lib]) };"
+				}
+				: do {
+					require Carp;
+					Carp::croak("'$lib' is not a recognized type constraint library")
+				};
 	} @{ $self->relations->{types} || [] };
 }
 
@@ -153,11 +163,6 @@ sub _mk_guard
 {
 	my $self = shift;
 	push @{$self->_guarded}, @_;
-}
-
-sub Moops::Keyword::__GUARD__::DESTROY
-{
-	$_[0]->() unless in_global_destruction;
 }
 
 1;
